12fb4c6642aa4538f84bd0eb378a1c93b7fd6561
Olenakas <121051082+Olenakas@users.noreply.github.com>
2023-06-02 09:59:21 +0000

    Deploying to gh-pages from @ Olenakas/goit-js-hw-09@a1050ae5223c82a7834ef517df60f4b32523887b 🚀

---
+++ 01-color-switcher.2ddf5dec.js
@@ -0,0 +1,2 @@
+!function(){var t=null,n=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");n.addEventListener("click",(function(){null===t&&(n.disabled=!0,t=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}),1e3))})),e.addEventListener("click",(function(){clearInterval(t),t=null,n.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.2ddf5dec.js.map
